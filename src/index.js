2eac5d335b8d5dfa486712d5c4851dd7bc2d53d7
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2022-06-27 00:04:51 -0300

    feat: moving objects with the scroll

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 /**
  * Catching interface's elements
  */
-let allElements = [...document.querySelectorAll('[id]')]
+const header = document.querySelector('.header-container')
+const allElements = [...document.querySelectorAll('[id]')]
 let elements = {}
 allElements.forEach(element => elements[element.id] = element)
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY
+    elements.stars.style.left = value * 0.25 + 'px'
+    elements.moon.style.top = value * 1.05 + 'px'
+    elements.mountains_behind.style.top = value * 0.5 + 'px'
+    elements.text.style.marginRight = value * 4 + 'px'
+    elements.text.style.marginTop = value * 1.5 + 'px'
+    elements.button.style.marginTop = value * 1.5 + 'px'
+    header.style.marginTop = value * 0.5 + 'px'
+})
